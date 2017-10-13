509a204a02c4bfdcb99d819ffcc185fc8b44a69b
Mike J Hewitt <mikeh@electroteach.com>
2017-10-13 16:20:58 +1100

    after lein uberjar

--- src/gist_notes/core.clj
+++ src/gist_notes/core.clj
@@ -5,28 +5,11 @@
 (require '[tentacles.gists :as gists])
 (require '[tentacles.repos :as repos])
 
-
-
-
-
-
-
-
-
-
-
 (defn retrieve-gists [user_name password] ;this works
   (let [auth (str user_name ":" password)]
     (count (gists/gists {:auth auth}))
   )
 )
-
-
-
-
-
-
-
 
 ;this works!
 (defn get-public-gists [that-user user_name password]
@@ -34,7 +17,7 @@
     (loop [page 1 amnt 0] (let [gist-count (count (gists/user-gists that-user {:auth auth :per-page 100 :page page}))] (if (= 100 (count (gists/user-gists that-user {:auth auth :per-page 100 :page page}))) 
                                                                                                                          
                                                                                                                          (recur (+ page 1) (+ amnt 100))
-                                                                                                                         (+ amnt gist-count))
+                                                                                                                        (+ amnt gist-count))
        )
     )
   )
@@ -52,10 +35,6 @@
     )
   )
 )
-
-
-
-
 
 (defn -main
   "I don't do a whole lot ... yet."
